c496d85496f11f33224b3542fe5985b5d9344f54
mcodt1702 <marco@mcnyc.net>
2020-06-30 12:38:43 -0400

    Updates

---
+++ precache-manifest.3d1569226aa2880868f6d0072618a221.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cadb49a232e0e3c8c126f829ba8f8e3",
+    "url": "/mcodt1702/kholsen.git/index.html"
+  },
+  {
+    "revision": "12dd3fb31ba50a3470ad",
+    "url": "/mcodt1702/kholsen.git/static/css/main.88e3b04a.chunk.css"
+  },
+  {
+    "revision": "a4318461a86746e7bfd3",
+    "url": "/mcodt1702/kholsen.git/static/js/2.282d5f1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mcodt1702/kholsen.git/static/js/2.282d5f1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12dd3fb31ba50a3470ad",
+    "url": "/mcodt1702/kholsen.git/static/js/main.22adc45a.chunk.js"
+  },
+  {
+    "revision": "c8ffe161813f0f9e0cb6",
+    "url": "/mcodt1702/kholsen.git/static/js/runtime-main.0c0012d3.js"
+  },
+  {
+    "revision": "50d82884301bcbe0bf82429f3007d514",
+    "url": "/mcodt1702/kholsen.git/static/media/jarblue.50d82884.jpg"
+  },
+  {
+    "revision": "7d1476c56ffc530dbb5bd83fef6266f1",
+    "url": "/mcodt1702/kholsen.git/static/media/logo-kholsen1.7d1476c5.png"
+  }
+]);
